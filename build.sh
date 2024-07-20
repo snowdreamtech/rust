bc8f0c20979429467f680e3e692d1b2bdebf68b9
snowdream <sn0wdr1am@qq.com>
2024-07-21 00:17:05 +0800

    feat: rust 1.79.0 with build-essential

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ GITHUB_PROJECT=ghcr.io/snowdreamtech/rust
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
     -t ${DOCKER_HUB_PROJECT}:build-essential-latest \
-    -t ${DOCKER_HUB_PROJECT}:build-essential-1.78.0 \
-    -t ${DOCKER_HUB_PROJECT}:build-essential-1.78 \
+    -t ${DOCKER_HUB_PROJECT}:build-essential-1.79.0 \
+    -t ${DOCKER_HUB_PROJECT}:build-essential-1.79 \
     -t ${DOCKER_HUB_PROJECT}:build-essential-1 \
     -t ${GITHUB_PROJECT}:build-essential-latest \
-    -t ${GITHUB_PROJECT}:build-essential-1.78.0 \
-    -t ${GITHUB_PROJECT}:build-essential-1.78 \
+    -t ${GITHUB_PROJECT}:build-essential-1.79.0 \
+    -t ${GITHUB_PROJECT}:build-essential-1.79 \
     -t ${GITHUB_PROJECT}:build-essential-1 \
     . \
     --push
