0810f449e2fd8407ce549320d8d4dea93dd37d1c
snowdream <sn0wdr1am@qq.com>
2024-07-20 23:28:18 +0800

    feat: rust 1.78.0

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-DOCKER_HUB_PROJECT=snowdreamtech/alpine
+DOCKER_HUB_PROJECT=snowdreamtech/rust
 
-GITHUB_PROJECT=ghcr.io/snowdreamtech/alpine
+GITHUB_PROJECT=ghcr.io/snowdreamtech/rust
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
     -t ${DOCKER_HUB_PROJECT}:latest \
-    -t ${DOCKER_HUB_PROJECT}:3.20.0 \
-    -t ${DOCKER_HUB_PROJECT}:3.20 \
-    -t ${DOCKER_HUB_PROJECT}:3 \
+    -t ${DOCKER_HUB_PROJECT}:1.78.0 \
+    -t ${DOCKER_HUB_PROJECT}:1.78 \
+    -t ${DOCKER_HUB_PROJECT}:1 \
     -t ${GITHUB_PROJECT}:latest \
-    -t ${GITHUB_PROJECT}:3.20.0 \
-    -t ${GITHUB_PROJECT}:3.20 \
-    -t ${GITHUB_PROJECT}:3 \
+    -t ${GITHUB_PROJECT}:1.78.0 \
+    -t ${GITHUB_PROJECT}:1.78 \
+    -t ${GITHUB_PROJECT}:1 \
     . \
     --push
